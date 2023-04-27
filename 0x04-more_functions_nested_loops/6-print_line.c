5f215df22d9fdcb59f3e36199d466adae8edc5e2
sallyibaid19 <sky.sally1998@gmail.com>
2023-04-27 11:38:02 -0700

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,14 +9,16 @@ void print_line(int n)
 	if (n <= 0)
 	{
 		_putchar('\n');
-	} 
+	}
 	else
 	{
 		int i;
+
 		for (i = 1; i <= n; i++)
 		{
 			_putchar('_');
 		}
-	_putchar('\n');
+
+		_putchar('\n');
 	}
 }
